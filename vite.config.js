1c72de2aadbacf033ec9c3e16599143c849b8cae
Gonzalo <gonzalohm93@gmail.com>
2025-03-10 18:02:43 +0100

    fix proxy to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target:
-          //  'http://localhost:4001',
-          'https://backmuseumapi.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // Separa los módulos de node_modules en chunks individuales para mejorar la carga
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0]
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development'
+
+  return {
+    base: '/',
+    plugins: [react()],
+    server: {
+      proxy: isDevelopment
+        ? {
+            '/api': {
+              target: 'http://localhost:4001',
+              changeOrigin: true,
+              secure: false
+            }
+          }
+        : {}
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          // Separa los módulos de node_modules en chunks individuales para mejorar la carga
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0]
+            }
           }
         }
       }
